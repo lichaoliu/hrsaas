f0381976f2f30c9a384d0c6e1f6ecd83cdad23d1
lichaoliu <1147149597@qq.com>
2022-07-09 15:30:39 +0800

    左侧菜单显示

---
+++ src/router/modules/employees.js
@@ -0,0 +1,15 @@
+import Layout from '@/layout'
+// 每个子模块 其实 都是外层是layout  组件位于layout的二级路由里面
+export default {
+  path: '/employees',
+  name: 'employees',
+  component: Layout,
+  children: [{
+    path: '', // 这里当二级路由的path什么都不写的时候 表示该路由为当前二级路由的默认路由
+    component: () => import('@/views/employees'),
+    meta: {
+      title: '员工', // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+      icon: 'people'
+    }
+  }]
+}
