8c26a5382fbdbe5bb4e09839e45764a1d63aadea
lichaoliu <1147149597@qq.com>
2022-07-09 20:09:16 +0800

    组织架构代码提交1

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-row type="flex"
+          justify="space-between"
+          class="tree-card"
+          align="middle"
+          style="height: 40px; width:100%">
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex"
+              justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 下拉菜单 -->
+          <el-dropdown>
+            <span>
+              操作<i class="el-icon-arrow-down" />
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除子部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+<script>
+export default {
+  props: {
+    treeNode: {
+      type: Object, // 对象类型
+      required: true // 要求对方使用您的组件的时候 必须传treeNode属性 如果不传 就会报错
+    },
+    isRoot: {
+      type: Boolean, // 用于控制是否显示编辑部门和删除部门
+      default: false
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <el-card>
+        <tree-tools :is-root="true"
+                    :tree-node="company" />
+        <el-tree :data="departs"
+                 :default-expand-all="true"
+                 :props="defaultProps">
+          <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
+          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
+          <tree-tools slot-scope="{ data }"
+                      :tree-node="data" />
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
 export default {
-
+  components: {
+    TreeTools
+  },
+  data () {
+    return {
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
+      defaultProps: {
+        label: 'name'
+      },
+      departs: [{ name: '总裁办', manager: 'a', children: [{ name: '董事会', manager: 'b' }] },
+      { name: '行政部', manager: 'c' }, { name: '人事部', manager: 'd' }]
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
