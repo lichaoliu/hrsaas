d8625af1d70dbc04cf62968eee0582d15c4c0abf
lichaoliu <1147149597@qq.com>
2022-07-09 12:31:43 +0800

    删除没用的路由

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -66,6 +66,7 @@ export default {
       this.$store.dispatch('app/toggleSideBar')
     },
     async logout () {
+      // 写不写await 都可以，方法不是异步的
       await this.$store.dispatch('user/logout')
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
     }
