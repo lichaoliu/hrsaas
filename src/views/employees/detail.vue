4ea2e0bdf03df4856d5d8cbb57eb4d26d7fad448
liulichao <liulichao@renrencai.com>
2022-07-16 13:44:33 +0800

    更新用户名和密码

---
+++ src/views/employees/detail.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="dashboard-container">
+        <div class="app-container">
+            <el-tabs>
+                <el-tab-pane label="登录账户设置">
+                    <el-form ref="userForm" :model="userInfo" :rules="rules" label-width="120px"
+                        style="margin-left: 120px; margin-top:30px">
+                        <el-form-item label="姓名:" prop="username">
+                            <el-input v-model="userInfo.username" style="width:300px"></el-input>
+                        </el-form-item>
+                        <el-form-item label="密码:" prop="password2">
+                            <el-input v-model="userInfo.password2" type="password" style="width:300px"></el-input>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="primary" @click="saveUser">更新</el-button>
+                        </el-form-item>
+                    </el-form>
+                </el-tab-pane>
+                <el-tab-pane label="个人详情">1</el-tab-pane>
+                <el-tab-pane label="岗位信息">1</el-tab-pane>
+            </el-tabs>
+        </div>
+    </div>
+</template>
+<script>
+import { getUserDetailById } from '@/api/user'
+import { saveUserDetailById } from '@/api/employees'
+
+export default {
+    data() {
+        return {
+            userId: this.$route.params.id, // 通过路由获取id  ?id=123 用query
+            userInfo: {
+                username: '',
+                password2: ''
+            },
+            rules: {
+                username: [{ required: true, message: '姓名不能为空', trigger: 'blur' }],
+                password2: [{ required: true, message: '密码不能为空', trigger: 'blur' },
+                { min: 6, max: 9, message: '密码长度6-9位', trigger: 'blur' }]
+            }
+        }
+    },
+    created() {
+        this.getUserDetailById()
+    },
+    methods: {
+        // 获取用户名 密码
+        async getUserDetailById() {
+            this.userInfo = await getUserDetailById(this.userId)
+        },
+        // 更新用户名 密码
+        saveUser() {
+            this.$refs.userForm.validate().then(async () => {
+                await saveUserDetailById({ ...this.userInfo, password: this.userInfo.password2 })
+                this.$message.success('修改用户信息成功')
+            })
+        }
+    }
+}
+</script>
+<styel>
+</styel>
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -4,85 +4,48 @@
       <page-tools :show-before="true">
         <span slot="before">共{{ page.total }}条记录</span>
         <div slot="after">
-          <el-button type="success"
-                     @click="$router.push('/import')">导入</el-button>
-          <el-button type="danger"
-                     @click="exportData">导出</el-button>
-          <el-button type="primary"
-                     @click="addEmployee">新增员工</el-button>
+          <el-button type="success" @click="$router.push('/import')">导入</el-button>
+          <el-button type="danger" @click="exportData">导出</el-button>
+          <el-button type="primary" @click="addEmployee">新增员工</el-button>
         </div>
       </page-tools>
       <el-card>
-        <el-table v-loading="loading"
-                  :border="true"
-                  :data="list">
-          <el-table-column label="序号"
-                           sortable=""
-                           type="index" />
-          <el-table-column label="姓名"
-                           sortable=""
-                           prop="username" />
-          <el-table-column label="工号"
-                           sortable=""
-                           prop="workNumber" />
-          <el-table-column label="聘用形式"
-                           sortable=""
-                           :formatter="formatEmployment"
-                           prop="formOfEmployment" />
-          <el-table-column label="部门"
-                           sortable=""
-                           prop="departmentName" />
+        <el-table v-loading="loading" :border="true" :data="list">
+          <el-table-column label="序号" sortable="" type="index" />
+          <el-table-column label="姓名" sortable="" prop="username" />
+          <el-table-column label="工号" sortable="" prop="workNumber" />
+          <el-table-column label="聘用形式" sortable="" :formatter="formatEmployment" prop="formOfEmployment" />
+          <el-table-column label="部门" sortable="" prop="departmentName" />
           <!-- 作用于插槽 + 过滤器 -->
-          <el-table-column label="入职时间"
-                           sortable=""
-                           prop="timeOfEntry">
+          <el-table-column label="入职时间" sortable="" prop="timeOfEntry">
             <template slot-scope="{row}">
               {{ row.timeOfEntry | formatDate }}
             </template>
           </el-table-column>
-          <el-table-column label="账户状态"
-                           sortable=""
-                           prop="enableState">
+          <el-table-column label="账户状态" sortable="" prop="enableState">
             <template slot-scope="{row}">
               <el-switch :value="row.enableState === 1" />
             </template>
 
           </el-table-column>
-          <el-table-column label="操作"
-                           sortable=""
-                           fixed="right"
-                           width="280">
+          <el-table-column label="操作" sortable="" fixed="right" width="280">
             <template slot-scope="{row}">
-              <el-button type="text"
-                         size="small">查看</el-button>
-              <el-button type="text"
-                         size="small">转正</el-button>
-              <el-button type="text"
-                         size="small">调岗</el-button>
-              <el-button type="text"
-                         size="small">离职</el-button>
-              <el-button type="text"
-                         size="small">角色</el-button>
-              <el-button type="text"
-                         size="small"
-                         @click="delEmployee(row.id)">删除</el-button>
+              <el-button type="text" size="small" @click="$router.push(`/employees/detail/${row.id}`)">查看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small" @click="delEmployee(row.id)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
-        <el-row type="flex"
-                justify="center"
-                align="middle"
-                style="height: 60px">
-          <el-pagination layout="prev, pager, next"
-                         :current-page="page.page"
-                         :page-size="page.size"
-                         :total="page.total"
-                         @current-change="changePage" />
+        <el-row type="flex" justify="center" align="middle" style="height: 60px">
+          <el-pagination layout="prev, pager, next" :current-page="page.page" :page-size="page.size" :total="page.total"
+            @current-change="changePage" />
         </el-row>
       </el-card>
       <!-- 放置弹层组件 -->
-      <AddEmployee :show-dialog.sync="showDialog"
-                   @refreshTableAndClose="refreshTableAndClose" />
+      <AddEmployee :show-dialog.sync="showDialog" @refreshTableAndClose="refreshTableAndClose" />
     </div>
   </div>
 </template>
@@ -95,7 +58,7 @@ import { formatDate } from '@/filters'
 
 export default {
   components: { AddEmployee },
-  data () {
+  data() {
     return {
       loading: false,
       list: [],
@@ -107,26 +70,26 @@ export default {
       showDialog: false
     }
   },
-  created () {
+  created() {
     this.getEmployeeList()
   },
   methods: {
-    async getEmployeeList () {
+    async getEmployeeList() {
       this.loading = true
       const { total, rows } = await getEmployeeList(this.page)
       this.page.total = total
       this.list = rows
       this.loading = false
     },
-    changePage (newPage) {
+    changePage(newPage) {
       this.page.page = newPage
       this.getEmployeeList()
     },
-    formatEmployment (row, column, cellValue, index) {
+    formatEmployment(row, column, cellValue, index) {
       const obj = EmployeeEnum.hireType.find(x => x.id === cellValue)
       return obj ? obj.value : '未知'
     },
-    async delEmployee (id) {
+    async delEmployee(id) {
       try {
         await this.$confirm('是否确定删除员工?')
         await delEmployee(id)
@@ -136,14 +99,14 @@ export default {
         console.log(error)
       }
     },
-    addEmployee () {
+    addEmployee() {
       this.showDialog = true
     },
-    refreshTableAndClose () {
+    refreshTableAndClose() {
       this.getEmployeeList()
       this.showDialog = false
     },
-    exportData () {
+    exportData() {
       import('@/vendor/Export2Excel').then(async excel => {
         const headers = {
           '手机号': 'mobile',
@@ -155,17 +118,21 @@ export default {
           '部门': 'departmentName'
         }
         const { rows } = await getEmployeeList({ page: 1, size: this.page.total })
+        const multiHeader = [['姓名', '主要信息', '', '', '', '', '部门']]
+        const merges = ['A1:A2', 'B1:F1', 'G1:G2']
         const data = this.formatJson(headers, rows)
         excel.export_json_to_excel({
           header: Object.keys(headers),
           data: data,
           filename: '员工表',
           autoWidth: true,
-          bookType: 'xlsx'
+          bookType: 'xlsx',
+          multiHeader,
+          merges
         })
       })
     },
-    formatJson (headers, rows) {
+    formatJson(headers, rows) {
       return rows.map(item => {
         return Object.keys(headers).map(key => {
           // 英文key
